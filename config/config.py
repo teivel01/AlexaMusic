e5bf87d2ff2693723661590eb9c804c936575e63
teivel01 <105483000+teivel01@users.noreply.github.com>
2023-06-25 03:24:39 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,7 +26,7 @@ SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "乛𝘼𝙇𝙀𝙓𝘼🕊️⃝🦋⁪⁬𝙈𝙐𝙎𝙄𝘾")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝘾𝙍𝘼𝙕𝙔𝘾𝘼𝙂𝙀 𝙈𝙐𝙎𝙄𝘾 BOT")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6174058850 5745099463").split()))
 
@@ -45,7 +45,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/CrazySisidlan")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
